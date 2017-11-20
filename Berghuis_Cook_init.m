4dec52f13a0598260dc5adcededacbdb9f3555ba
Grafton <grafton.cook@gmail.com>
2017-11-20 11:23:33 -0500

    Update Berghuis_Cook_init.m
    Took a fresh look at the speaker system properties and added units in comments next to certain lines for clarity.

--- Berghuis_Cook_init.m
+++ Berghuis_Cook_init.m
@@ -3,19 +3,19 @@
 
 % Set environmental variables.
 dt = 1e-4;
-sinHz = [500 1000 1500];
-sinFreq = sinHz.*2*pi;
+sinHz = [500 1000 1500];  
+sinFreq = sinHz.*2*pi;  % (rad/s)
 
-% Establish pressure. Units: db (Hz), Pr (Pa), P (Pa).
+% Calculate pressure. Units: db (Hz), Pr (Pa), P (Pa).
 db = 70;
 Pr = 20e-6;
 P = Pr*10^(db/20);
 
 % Speaker system properties.
-resFreq = 1000;
-m = 0.125;
+resFreq = 1000;   % (Hz)
+m = 0.125;  % (kg)
 zeta = 0.125;
-wn = 2*pi*resFreq;
+wn = 2*pi*resFreq;  % (rad/s)
 k = m * wn^2;
 c = 2*zeta*sqrt(m*k);
 
