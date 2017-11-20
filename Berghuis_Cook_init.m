b250a29d2fef9cf61929756412501535d526b9be
Grafton <grafton.cook@gmail.com>
2017-11-20 18:43:59 -0500

    Updated Simulink model.
    Model updated to reflect input from Dr. Richards.

--- Berghuis_Cook_init.m
+++ Berghuis_Cook_init.m
@@ -3,25 +3,25 @@
 
 % Set environmental variables.
 dt = 1e-4;
-sinHz = [500 1000 1500];  
-sinFreq = sinHz.*2*pi;  % (rad/s)
+sinHz = [500 1000 1500];
+sinFreq = sinHz.*2*pi;
 
-% Calculate pressure. Units: db (Hz), Pr (Pa), P (Pa).
+% Establish pressure. Units: db (Hz), Pr (Pa), P (Pa).
 db = 70;
 Pr = 20e-6;
 P = Pr*10^(db/20);
 
 % Speaker system properties.
-resFreq = 1000;   % (Hz)
-m = 0.125;  % (kg)
+resFreq = 1000;
+m = 0.125;
 zeta = 0.125;
-wn = 2*pi*resFreq;  % (rad/s)
+wn = 2*pi*resFreq;
 k = m * wn^2;
 c = 2*zeta*sqrt(m*k);
 
 % Tuneable parameters.
-alpha12 = 20;     % Convergence coefficient for questions 1 & 2.
+alpha12 = 1e6;     % Convergence coefficient for questions 1 & 2.
 % alpha3 = ;        % Convergence coefficient for question 3.
 
 % Open the Simulink file.
-sim('Berghuis_Cook')
+% sim('Berghuis_Cook')
